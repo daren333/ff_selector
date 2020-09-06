d8d4ccf681df3470672118b66dd2112146edaee9
daren333 <daren.ansher@gmail.com>
2020-09-05 21:29:05 -0400

    finit

--- ff_selector.py
+++ ff_selector.py
@@ -2,9 +2,15 @@ import random
 
 random.seed(12)
 
-dc = ["daren", "meghan"]
-md = ["brendan", "nick"]
+dm = ["daren", "meghan"]
+bn = ["brendan", "nick"]
+rest = ["Garrett", "Ben_Bailey", "Kris", "Ed"]
 
 
-print(random.choice(dc) + " wins")
-print(random.choice(md) + " wins")
+print("Meghan vs. Daren: %s wins" % random.choice(dm))
+print("Brendan vs. Nick: %s wins" % random.choice(bn))
+
+for i in range(4):
+    choice = random.choice(rest)
+    print(choice)
+    rest.pop(rest.index(choice))
